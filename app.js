53e6f3b3c2701f5b95983082eb94c370ffff7562
Orlando2357 <orlando.perez.2357@gmail.com>
2025-08-19 17:13:14 -0600

    Actualizacion de la funcion agregarAmigo

--- app.js
+++ app.js
@@ -3,8 +3,13 @@ let listaDeAmigos = [];
 function agregarAmigo() {
     let amigo = document.getElementById('amigo').value;
     if (amigo == '') {
-        alert('Valor invalido');
+        alert('Por favor, inserte un nombre');
     } else {
-        listaDeAmigos.push(amigo)
+        listaDeAmigos.push(amigo);
+        limpiarCaja();
     }
+}
+
+function limpiarCaja() {
+    document.querySelector('#amigo').value = '';
 }
